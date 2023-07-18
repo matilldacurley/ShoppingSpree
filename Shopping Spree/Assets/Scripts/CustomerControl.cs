89df2b704456839b66ce2415e05a53511938c689
goldfish-pow <113395167+goldfish-pow@users.noreply.github.com>
2023-07-18 09:22:30 -0700

    test2

--- Shopping Spree/Assets/Scripts/CustomerControl.cs
+++ Shopping Spree/Assets/Scripts/CustomerControl.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CustomerControl : MonoBehaviour
 {
+    public GameObject[] items;
+    public int wantItem;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        wantItem = Random.Range(0, items.length - 1);
     }
 
     // Update is called once per frame
