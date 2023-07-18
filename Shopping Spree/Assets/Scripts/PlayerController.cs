3effd1dd8b91502ba017cc6a4057f6f228a9451d
ttedon <Dhruva.madhan@gmail.com>
2023-07-18 10:12:02 -0700

    Player Movement & Collision

--- Shopping Spree/Assets/Scripts/PlayerController.cs
+++ Shopping Spree/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float speed = 1f;
+    float v;
+    float h;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        v = Input.GetAxisRaw("Vertical");
+        h = Input.GetAxisRaw("Horizontal");
+        //rb.AddForce(new Vector2(h * speed, v * speed));
+        rb.velocity = new Vector2 (h * speed, v * speed);
     }
 }
