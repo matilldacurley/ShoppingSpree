579bc808082f4f57ced6ff9cbcc9b258e9915487
matilldacurley <mcc9851@nyu.edu>
2023-07-18 13:11:31 -0400

    made collisions work and added some pngs

--- Shopping Spree/Assets/Scripts/PlayerController.cs
+++ Shopping Spree/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+
+    public float runSpeed = 20.0f;
+
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
     }
 }
