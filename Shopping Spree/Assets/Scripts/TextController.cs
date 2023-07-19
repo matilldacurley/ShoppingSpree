dcb742d9d01e10be40884bae4837d50cedce0bcd
ttedon <Dhruva.madhan@gmail.com>
2023-07-18 19:45:41 -0700

    Update TextController.cs

--- Shopping Spree/Assets/Scripts/TextController.cs
+++ Shopping Spree/Assets/Scripts/TextController.cs
@@ -16,7 +16,7 @@ public class TextController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.M))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             isGameActive = true;
             titleScreen.SetActive(false);
