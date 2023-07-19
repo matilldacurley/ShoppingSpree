87b2f707c163926205a5e8e9e3bf89c3632184b0
goldfish-pow <113395167+goldfish-pow@users.noreply.github.com>
2023-07-19 15:15:15 -0700

    Changes
    Made the item stay in the thought bubble

--- Shopping Spree/Assets/Scripts/ThoughtBubble.cs
+++ Shopping Spree/Assets/Scripts/ThoughtBubble.cs
@@ -6,23 +6,29 @@ public class ThoughtBubble : MonoBehaviour
 {
     public GameObject[] items;
     public int rand;
-    
+    public GameObject pitem;
 
     // Start is called before the first frame update
     void Start()
     {
-        rand = Random.Range(0, 3);
+        rand = Random.Range(0, 2);
+        print(rand);
+        pitem = Instantiate(items[rand], new Vector3(transform.position.x, transform.position.y, -0.1f), items[rand].transform.rotation);
+        pitem.SetActive(false);
+        pitem.transform.SetParent(transform.parent);
         //name = 
     }
 
     public void disPlay()
     {
-        Instantiate(items[0], transform.position, items[0].transform.rotation);
+        //items[0].transform.SetParent(transform.parent);
+        //Instantiate(items[0], transform.position, items[0].transform.rotation);
+        pitem.SetActive(true);
     }
 
     public GameObject pickedItem()
     {
-        return items[0];
+        return items[rand];
     }
 
     // Update is called once per frame
