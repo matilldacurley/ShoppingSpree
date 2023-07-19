d76bfc0f61e6dced8560653d4069bf5dda3ec557
goldfish-pow <113395167+goldfish-pow@users.noreply.github.com>
2023-07-19 07:51:57 -0700

    Custumer bar and thoughtBubble

--- Shopping Spree/Assets/Scripts/CustomerControl.cs
+++ Shopping Spree/Assets/Scripts/CustomerControl.cs
@@ -6,11 +6,14 @@ public class CustomerControl : MonoBehaviour
 {
     public GameObject[] items;
     public int wantItem;
+    public GameObject bar;
+    public GameObject thoughtBubble;
 
     // Start is called before the first frame update
     void Start()
     {
-        wantItem = Random.Range(0, items.length - 1);
+        wantItem = Random.Range(0, 3 - 1);
+        bar.SetActive(true);
     }
 
     // Update is called once per frame
