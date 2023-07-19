38d8fe886389efcb97080d0670fc6e204458a1cd
goldfish-pow <113395167+goldfish-pow@users.noreply.github.com>
2023-07-19 12:29:40 -0700

    Cus can get item from thought

--- Shopping Spree/Assets/Scripts/ThoughtBubble.cs
+++ Shopping Spree/Assets/Scripts/ThoughtBubble.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class ThoughtBubble : MonoBehaviour
 {
     public GameObject[] items;
+    public int rand;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        //Instantiate(items[0], this.transform.position, items[0].transform.rotation);
+        rand = Random.Range(0, 3);
+        //name = 
     }
 
     public void disPlay()
     {
         Instantiate(items[0], transform.position, items[0].transform.rotation);
+    }
+
+    public GameObject pickedItem()
+    {
+        return items[0];
     }
 
     // Update is called once per frame
