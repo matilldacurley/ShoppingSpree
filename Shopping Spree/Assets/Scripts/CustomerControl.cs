104215517885435f2637d78e1270be1d829e1dba
goldfish-pow <113395167+goldfish-pow@users.noreply.github.com>
2023-07-19 10:50:42 -0700

    finished up thought bubble

--- Shopping Spree/Assets/Scripts/CustomerControl.cs
+++ Shopping Spree/Assets/Scripts/CustomerControl.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class CustomerControl : MonoBehaviour
 {
-    public GameObject[] items;
-    public int wantItem;
+    public GameObject wantItem;
+    public int collect;
     public GameObject bar;
     public GameObject thoughtBubble;
 
     // Start is called before the first frame update
     void Start()
     {
-        wantItem = Random.Range(0, 3 - 1);
         bar.SetActive(true);
-        wantItem = Random.Range(0, items.Length - 1);
+        thoughtBubble.SetActive(true);
     }
 
     // Update is called once per frame
