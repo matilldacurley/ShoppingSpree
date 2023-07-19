6e3720820d0a855058af625e50fead6f198c760d
ttedon <Dhruva.madhan@gmail.com>
2023-07-19 11:39:52 -0700

    Little Change

--- Shopping Spree/Assets/Scripts/Dhruva's Scripts O_O/SpawnPoint.cs
+++ Shopping Spree/Assets/Scripts/Dhruva's Scripts O_O/SpawnPoint.cs
@@ -7,7 +7,7 @@ public class SpawnPoint : MonoBehaviour
 	Vector3 mousepos;
 	public Transform target;
 	public Transform player;
-	Vector3 object_pos;
+	Vector3 objectpos;
 	float angle;
 	public float pivotOffset = -0.135f;
 
@@ -15,9 +15,9 @@ public class SpawnPoint : MonoBehaviour
 	{
 		transform.position = new Vector3(player.transform.position.x, player.transform.position.y + pivotOffset, 0);
 		mousepos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
-		object_pos = Camera.main.WorldToScreenPoint(new Vector3(target.position.x, target.position.y, 0));
-		mousepos.x = mousepos.x - object_pos.x;
-		mousepos.y = mousepos.y - object_pos.y;
+		objectpos = Camera.main.WorldToScreenPoint(new Vector3(target.position.x, target.position.y, 0));
+		mousepos.x = mousepos.x - objectpos.x;
+		mousepos.y = mousepos.y - objectpos.y;
 		angle = Mathf.Atan2(mousepos.y, mousepos.x) * Mathf.Rad2Deg;
 		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 	}
