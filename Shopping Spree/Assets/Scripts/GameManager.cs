6136cd9a160e3cfbdb1d85fb227f975e2ed2800b
ttedon <Dhruva.madhan@gmail.com>
2023-07-20 08:53:15 -0700

    MUSICCCCCCC :D

--- Shopping Spree/Assets/Scripts/GameManager.cs
+++ Shopping Spree/Assets/Scripts/GameManager.cs
@@ -6,10 +6,15 @@ public class GameManager : MonoBehaviour
 {
     private bool isGameActive = false;
     public GameObject titleScreen;
+    public AudioClip[] music;
+    public AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         titleScreen.SetActive(true);
+        audioSource.clip = music[0];
+        audioSource.Play();
     }
 
 
@@ -18,8 +23,11 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            audioSource.Stop();
             isGameActive = true;
             titleScreen.SetActive(false);
+            audioSource.clip = music[1];
+            audioSource.Play();
         }
     }
 }
