862b0fd0c46b13e73b0134cbb76452e6f4567ca1
matilldacurley <mcc9851@nyu.edu>
2023-07-20 09:16:23 -0400

    working on inventory

--- Shopping Spree/Assets/Scripts/Matilda's Scripts/DraggableItem.cs
+++ Shopping Spree/Assets/Scripts/Matilda's Scripts/DraggableItem.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+
+    [HideInInspector] public Item item;
+
+    public Image image;
+    [HideInInspector] public Transform parentAfterDrag;
+
+    public void InitializeItem(Item newItem)
+    {
+        item = newItem;
+        image.sprite = newItem.image;
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("Begin drag");
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+        image.raycastTarget = false;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -19,6 +36,8 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("End drag");
+        transform.SetParent(parentAfterDrag);
+        image.raycastTarget = true;
     }
 
 
