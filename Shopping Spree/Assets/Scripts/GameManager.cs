8f5d833415c9d81a5588d1f99e82aba15694b477
ttedon <Dhruva.madhan@gmail.com>
2023-07-20 08:58:47 -0700

    Game-Over Half-Implemented :(

--- Shopping Spree/Assets/Scripts/GameManager.cs
+++ Shopping Spree/Assets/Scripts/GameManager.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private bool isGameActive = false;
+    public int lives;
     public GameObject titleScreen;
+    public GameObject gameOverScreen;
     public AudioClip[] music;
     public AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
+        gameOverScreen.SetActive(false);
         titleScreen.SetActive(true);
         audioSource.clip = music[0];
         audioSource.Play();
@@ -27,6 +30,14 @@ public class GameManager : MonoBehaviour
             isGameActive = true;
             titleScreen.SetActive(false);
             audioSource.clip = music[1];
+            audioSource.Play();
+        }
+        if(lives <= 0)
+        {
+            audioSource.Stop();
+            isGameActive = false;
+            gameOverScreen.SetActive(true);
+            audioSource.clip = music[2];
             audioSource.Play();
         }
     }
